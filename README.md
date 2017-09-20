0b25c8c84f05ee8fcf1de398f3d9a904126744e9
Christian Lorenz <christian.lorenz@hmmh.de>
2017-09-20 10:29:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# myspringconfigserver
---
+++ src/main/java/com/example/myspringconfigserver/MySpringConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.myspringconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MySpringConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringConfigServerApplication.class, args);
+	}
+}
